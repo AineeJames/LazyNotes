1f91757c8d3d6258162d5b0e5ce76c774fa8e2eb
Aiden Olsen <olsenai@oregonstate.edu>
2022-03-10 18:34:05 -0800

    init code

---
+++ main.py
@@ -0,0 +1,70 @@
+import cv2
+import time # for framerate
+import numpy as np
+from tqdm import tqdm
+from os import listdir
+from os.path import isfile, join
+from difPy import dif
+import subprocess
+import math
+
+onlyfiles = [f for f in listdir("slides") if isfile(join("slides", f))]
+print(f"Number of files to capture: {len(onlyfiles)}")
+
+cropnum = 0
+
+for i in tqdm(range(len(onlyfiles))):
+
+    img = cv2.imread(f"slides/{onlyfiles[i]}", cv2.IMREAD_UNCHANGED)
+    
+    #print('Original Dimensions : ',img.shape)
+    
+    scale_percent = 10 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    
+    # resize image
+    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    #print('New Dimensions : ',img.shape)
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    thresh_inv = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+
+    # Blur the image
+    blur = cv2.GaussianBlur(thresh_inv,(1,1),0)
+
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    # cv2.imshow("thresh", thresh)
+
+    # find contours
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    mask = np.ones(img.shape[:2], dtype="uint8") * 255
+    for c in contours:
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(c)
+        if w*h>50000/6:
+            cv2.rectangle(mask, (x, y), (x+w, y+h), (0, 0, 255), -1)
+            # print(f"x: {x}, y: {y}, w: {w}, h: {h}\n")
+            cropped_box = img[y:y+h, x:x+w]
+            
+            cropnum += 1
+            cv2.imwrite(f"extracted/crop_{cropnum}.jpg", cropped_box)
+
+            # cv2.imshow("crop", cropped_box)
+            # cv2.waitKey(0)
+
+    res_final = cv2.bitwise_and(img, img, mask=cv2.bitwise_not(mask))
+
+print("Deleting duplicate files, please wait...")
+search = dif("extracted", delete=True, silent_del=True)
+
+args = ['python', 'packer.py', '--input_dir', 'extracted', '--width', '2500', '--aspect', f'{math.sqrt(2)}', '--border', '3']
+subprocess.run(args)
+
+    # cv2.imshow("boxes", mask)
+    # cv2.imshow("final image", res_final)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
---
+++ packer.py
@@ -0,0 +1,46 @@
+import cv2
+# import rpack
+import os
+import glob
+from rectpack import newPacker
+import pickle
+import numpy as np
+import argparse
+parser = argparse.ArgumentParser(description='Montage creator with rectpack')
+parser.add_argument('--width', help='Output image width', default=5200, type=int)
+parser.add_argument('--aspect', help='Output image aspect ratio, \
+    e.g. height = <width> * <aspect>', default=1.0, type=float)
+parser.add_argument('--output', help='Output image name', default='output.png')
+parser.add_argument('--input_dir', help='Input directory with images', default='./')
+parser.add_argument('--debug', help='Draw "debug" info', default=False, type=bool)
+parser.add_argument('--border', help='Border around images in px', default=2, type=int)
+args = parser.parse_args()
+files = sum([glob.glob(os.path.join(args.input_dir, '*.' + e)) for e in ['jpg', 'jpeg', 'png']], [])
+print('found %d files in %s' % (len(files), args.input_dir))
+print('getting images sizes...')
+sizes = [(im_file, cv2.imread(im_file).shape) for im_file in files]
+# NOTE: you could pick a different packing algo by setting pack_algo=..., e.g. pack_algo=rectpack.SkylineBlWm
+packer = newPacker(rotation=False)
+for i, r in enumerate(sizes):
+    packer.add_rect(r[1][1] + args.border * 2, r[1][0] + args.border * 2, rid=i)
+out_w = args.width
+aspect_ratio_wh = args.aspect
+out_h = int(out_w * aspect_ratio_wh)
+packer.add_bin(out_w, out_h)
+print('packing...')
+packer.pack()
+output_im = np.full((out_h, out_w, 3), 255, np.uint8)
+used = []
+for rect in packer.rect_list():
+    b, x, y, w, h, rid = rect
+    used += [rid]
+    orig_file_name = sizes[rid][0]
+    im = cv2.imread(orig_file_name, cv2.IMREAD_COLOR)
+    output_im[out_h - y - h + args.border : out_h - y - args.border, x + args.border:x+w - args.border] = im
+    if args.debug:
+        cv2.rectangle(output_im, (x,out_h - y - h), (x+w,out_h - y), (255,0,0), 3)
+        cv2.putText(output_im, "%d"%rid, (x, out_h - y), cv2.FONT_HERSHEY_PLAIN, 3.0, (0,0,255), 2)
+print('used %d of %d images' % (len(used), len(files)))
+print('writing image output %s:...' % args.output)
+cv2.imwrite(args.output, output_im)
+print('done.')
