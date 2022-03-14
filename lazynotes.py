4071563715b9a7646cd5e2084d618afb8fd28302
Aiden Olsen <olsenai@oregonstate.edu>
2022-03-14 01:37:18 -0700

    began work on GUI

--- lazynotes.py
+++ lazynotes.py
@@ -11,3 +11,24 @@ import glob
 from pathlib import Path
 from fpdf import FPDF
 import sys
+import tkinter as tk
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+# All the stuff inside your window.
+layout = [  [sg.Text("Choose a file: "), sg.FileBrowse(file_types = (("PDF Files", "*.pdf"),))],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('LazyNotes', layout)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
